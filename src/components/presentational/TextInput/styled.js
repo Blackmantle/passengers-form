8ef46833d6451254551ce318c6dd3d1194b5fd7f
BlackMantle <Dimasikylll@mail.ru>
2021-10-19 10:25:30 +0300

    feat: add coloring input background on error

--- src/components/presentational/TextInput/styled.js
+++ src/components/presentational/TextInput/styled.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 
 export const Input = styled.input`
   width: 100%;
   margin-top: 5px;
   padding: 8px;
   border: 1px solid ${props => props.error ? 'red' : 'silver'};
+  background: ${props => props.error ? lighten(0.47, 'red') : 'white'};
   border-radius: 4px;
   &:disabled {
     background: lavender;
